ecb38e3ed17302ab2dfcc643bbe37ca00a512ee0
Brian Kang <bhkangw@gmail.com>
2017-12-05 14:41:51 -0800

    Initial Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PokeInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        [Route("pokeinfo/{id}")]
+        public IActionResult GetInfo(int id)
+        {
+            var PokeObject = new Pokemon();
+
+            WebRequest.GetPokemonDataAsync(id, PokeResponse =>
+            {
+                PokeObject = PokeResponse;
+            }).Wait();
+            ViewBag.Pokemon = PokeObject;
+            return View();
+        }
+    }
+}
